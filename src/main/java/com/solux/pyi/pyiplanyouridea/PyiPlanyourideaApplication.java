d94473e952627c69d6d2dffdbb66d64429889bde
meli <melitina915@gmail.com>
2024-01-08 01:39:59 +0900

    first commit

---
+++ src/main/java/com/solux/pyi/pyiplanyouridea/PyiPlanyourideaApplication.java
@@ -0,0 +1,13 @@
+package com.solux.pyi.pyiplanyouridea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyiPlanyourideaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyiPlanyourideaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solux/pyi/pyiplanyouridea/PyiPlanyourideaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solux.pyi.pyiplanyouridea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyiPlanyourideaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
